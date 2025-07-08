affe5c563f57714623ae98e0d89a1c439c57f0aa
Daniel Page <dpage105@gmail.com>
2025-07-07 21:09:19 -0500

    Added IProducRepository and ProductRepository; Add Product folder in Views; Added Index.cshtml In View/Product folder

---
+++ Testing/ProductRepository.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Data;
+using System.Collections.Generic;
+using Dapper;
+using Testing.Models;
+
+namespace Testing
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly IDbConnection _conn;
+
+        public ProductRepository(IDbConnection conn)
+        {
+            _conn = conn;
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _conn.Query<Product>("SELECT * FROM PRODUCTS;");
+        }
+    }
+}
